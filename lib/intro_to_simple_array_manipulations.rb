ffb6d51617305b18c24ae75e7496383fd56aabc4
Will Lytle <wlytle89@gmail.com>
2020-08-13 00:44:02 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,26 @@
+def using_concat(first_array, second_array)
+  first_array.concat second_array
+end
+
+def using_insert(array, element)
+  array.insert(4, element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, n)
+  array.delete_at(n)
+end
+
+
+
